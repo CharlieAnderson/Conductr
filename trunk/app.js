2b82f8131c4e4bc61a20b5df84237ae79d78f979
Charlie Anderson <charlieanderson13@gmail.com>
2016-01-12 13:00:23 -0500

    .
    .

--- trunk/app.js
+++ trunk/app.js
@@ -13,8 +13,9 @@ var express = require('express'),
   api = require('./routes/api'),
   http = require('http').Server(app),
   path = require('path'),
-  io = require('socket.io')(http);
-
+  io = require('socket.io')(http),
+  passport = require('passport');
+  LocalStrategy = require('passport-local').Strategy;
 //var server = http.createServer(app);
 
 /**
@@ -30,6 +31,10 @@ app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// passport initialization
+app.use(passport.initialize());
+app.use(passport.session());
+
 var env = process.env.NODE_ENV || 'development';
 
 // development only
@@ -42,7 +47,6 @@ if (env === 'production') {
   // TODO
 }
 
-
 /**
  * Routes
  */
@@ -54,6 +58,20 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/login', loginGet);
+
+function loginGet(req, res){
+  if(req.user){
+    // already logged in
+    res.redirect('/');
+  } else {
+    // not logged in, show the login form, remember to pass the message
+    // for displaying when error happens
+    res.render('login', { message: req.session.messages });
+    // and then remember to clear the message
+    req.session.messages = null;
+  }
+}
 app.get('/partials/:name', routes.partials);
 
 // JSON API
@@ -61,8 +79,6 @@ app.get('/api/name', api.name);
 
 // redirect all others to the index (HTML5 history)
 app.get('*', routes.index);
-
-
 
 io.on('connection', function(socket) {
   console.log('A user has connected');
@@ -84,12 +100,82 @@ http.listen(3000, function() {
 
 
 
+passport.use(new LocalStrategy({
+    // set the field name here
+    usernameField: 'username',
+    passwordField: 'password'
+  },
+  function(username, password, done) {
+    /* get the username and password from the input arguments of the function */
 
-/**
- * Start Server
- */
-/*
-http.createServer(app).listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
+    // query the user from the database
+    // don't care the way I query from database, you can use
+    // any method to query the user from database
+    User.find( { where: {username: username}} )
+      .success(function(user){
+      
+        if(!user)
+          // if the user is not exist
+          return done(null, false, {message: "The user is not exist"});
+        else if(!hashing.compare(password, user.password))
+          // if password does not match
+          return done(null, false, {message: "Wrong password"});
+        else
+          // if everything is OK, return null as the error
+          // and the authenticated user
+          return done(null, user);
+        
+      })
+      .error(function(err){
+        // if command executed with error
+        return done(err);
+      });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
 });
-*/
+
+passport.deserializeUser(function(id, done) {
+  // query the current user from database
+  User.find(id)
+    .success(function(user){
+        done(null, user);
+    }).error(function(err){
+        done(new Error('User ' + id + ' does not exist'));
+    });
+});
+
+app.post('/login', loginPost);
+
+function loginPost(req, res, next) {
+  // ask passport to authenticate
+  passport.authenticate('local', function(err, user, info) {
+    if (err) {
+      // if error happens
+      return next(err);
+    }
+    
+    if (!user) {
+      // if authentication fail, get the error message that we set
+      // from previous (info.message) step, assign it into to
+      // req.session and redirect to the login page again to display
+      req.session.messages = info.message;
+      return res.redirect('/login');
+    }
+
+    // if everything's OK
+    req.logIn(user, function(err) {
+      if (err) {
+        req.session.messages = "Error";
+        return next(err);
+      }
+
+      // set the message
+      req.session.messages = "Login successfully";
+      return res.redirect('/');
+    });
+    
+  })(req, res, next);
+}
